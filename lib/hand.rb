85e747b33dd273e5f728150c18585960f7c0b8f9
App Academy Student <student@appacademy.io>
2013-09-27 18:14:58 -0700

    Add hand evaluation and tests.

--- lib/hand.rb
+++ lib/hand.rb
@@ -1,13 +1,19 @@
 class Hand
-  attr_accessor :cards
+  attr_accessor :cards, :hand_type
+
+  HAND_CHECK_METHODS = [:royal_flush?, :straight_flush?, :four_of_a_kind?,
+                        :full_house?, :flush?, :straight?, :three_of_a_kind?,
+                        :two_pair?, :pair?]
 
   def initialize
     @cards = []
+    @hand_type = Hash.new
   end
 
   def add_card(card)
     return false if @cards.count >= 5
     @cards << card
+    @cards.sort_by! { |card| card.value }
     true
   end
 
@@ -15,6 +21,91 @@ class Hand
     return false if @cards.count == 0
     @cards.delete_at(card_number)
     true
+  end
+
+  def find_best_cards
+    best_cards = false
+
+    HAND_CHECK_METHODS.each do |method|
+      best_cards = send(method)
+
+      unless best_cards == false
+        @hand_type[:best_cards] = best_cards
+        @hand_type[:type] = method.to_s.chop
+        return true
+      end
+    end
+
+    @hand_type[:best_cards] = [@cards.last]
+    @hand_type[:type] = "high_card"
+  end
+
+
+  def royal_flush?
+    high_card = (@cards.map { |card| card.value }).max
+    straight_flush? && high_card == 14 ? @cards.dup : false
+  end
+
+  def straight_flush?
+    flush? && straight? ? @cards.dup : false
+  end
+
+  def flush?
+    suits = @cards.map { |card| card.suit }
+    suits.uniq.length == 1 ? @cards.dup : false
+  end
+
+  def straight?
+    values = @cards.map { |card| card.value }
+    values.uniq!
+    return false if values.count < 5
+    values.last - values.first == 4 ? @cards.dup : false
+  end
+
+  def full_house?
+    values = @cards.map { |card| card.value }
+    type1 = values.first == values[2] && values[3] == values.last
+    type2 = values.first == values[1] && values[2] == values.last
+    type1 || type2 ? @cards.dup : false
+  end
+
+  def three_of_a_kind?
+    vals = @cards.map { |card| card.value }
+    return @cards[0..2] if vals[0] == vals[2]
+    return @cards[1..3] if vals[1] == vals[3]
+    return @cards[2..4] if vals[2] == vals[4]
+    false
+  end
+
+  def four_of_a_kind?
+    vals = @cards.map { |card| card.value }
+    return @cards[0..3] if vals[0] == vals[3]
+    return @cards[1..4] if vals[1] == vals[4]
+    false
+  end
+
+  def two_pair?
+    pairs = all_pairs
+    pairs.length == 2 ? pairs.flatten : false
+  end
+
+  def pair?
+    pairs = all_pairs
+    pairs.length == 1 ? pairs.flatten : false
+  end
+
+  def all_pairs
+    pairs = []
+    cards = @cards.dup
+    cards.sort_by! { |card| card.value }
+    card = cards.pop
+    until cards.empty?
+      if card.value == cards.last.value
+        two_pair << [card, cards.pop]
+      end
+      card = cards.pop
+    end
+    pairs
   end
 
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,5 +1,6 @@
 require 'rspec'
 require_relative '../lib/hand.rb'
+require_relative '../lib/card.rb'
 
 describe Hand do
   subject(:hand) { Hand.new }
@@ -11,25 +12,25 @@ describe Hand do
   end
 
   describe "#add_card" do
-    let(:card) { double }
+    let(:card1) { Card.new(2, :club) }
 
     it "should have more cards after add_card" do
       previous_count = hand.cards.count
-      hand.add_card(card)
+      hand.add_card(card1)
       hand.cards.count.should > previous_count
     end
 
     it "should not have more than 5 cards" do
-      6.times { hand.add_card(card) }
+      6.times { hand.add_card(card1) }
       expect(hand.cards.count).to eq(5)
     end
   end
 
   describe "#discard_card" do
-    let(:card) { double }
+    let(:card1) { Card.new(2, :club) }
 
     it "should have fewer cards after discard_card" do
-      5.times { hand.add_card(card) }
+      5.times { hand.add_card(card1) }
       previous_count = hand.cards.count
       hand.discard_card(0)
       expect(hand.cards.count).to be < previous_count
@@ -39,6 +40,24 @@ describe Hand do
       expect(hand.discard_card(0)).to eq(false)
     end
 
+  end
+
+  describe "#find_best_cards" do
+    let(:card1) { Card.new(10, :heart) }
+    let(:card2) { Card.new(11, :heart) }
+    let(:card3) { Card.new(12, :heart) }
+    let(:card4) { Card.new(13, :heart) }
+    let(:card5) { Card.new(14, :heart) }
+
+    it "should find the best hand it can from its cards" do
+      hand.add_card(card1)
+      hand.add_card(card2)
+      hand.add_card(card3)
+      hand.add_card(card4)
+      hand.add_card(card5)
+      hand.find_best_cards
+      expect(hand.hand_type[:type]).to eq("royal_flush")
+    end
   end
 
 
